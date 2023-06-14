b710fb12ab73e958ea071291351e58ae5ab61ffc
sjamescarter <stephenjamescarter@gmail.com>
2023-06-14 16:26:41 -0500

    add create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :first_name, :last_name, :job_title)
+  end
 end
