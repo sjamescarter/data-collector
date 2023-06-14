275bde8991fd33e0eefcadd223c07c90ecd4fa60
sjamescarter <stephenjamescarter@gmail.com>
2023-06-14 16:10:30 -0500

    fix regex typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, :password, :password_confirmation, :first_name, :last_name, :job_title, presence: true
-  validates :email, format: { with: /^[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/}, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 } 
+  validates :email, format: { with: /\A[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}\z/}, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 } 
   validates :password, length: { minimum: 8 }
 end
