e5a285fc620c72d4af60f947adf56342ae72c560
sjamescarter <stephenjamescarter@gmail.com>
2023-06-14 16:07:13 -0500

    add User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Seeding 🌱"
+
+# User Seed Data
+User.create(email: "slimjim25@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Jim", last_name: "Thompson", job_title: "DHH Teacher")
+User.create(email: "lannette.simes@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Lannette", last_name: "Simes", job_title: "Physical Therapist")
+User.create(email: "janice-jones@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Janice", last_name: "Jones", job_title: "Speech/Language Pathologist")
+
+puts "Done Seeding 🌾"
