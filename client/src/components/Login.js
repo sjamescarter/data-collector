71b04eae18b9a82346137b39b9e63b56d54f48a7
sjamescarter <stephenjamescarter@gmail.com>
2023-06-16 15:32:57 -0500

    update SignIn/SignUp logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import SignUpForm from './SignUpForm';
+
 function Login({ onLogin }) {
+    const [showLogin, setShowLogin] = useState(true)
+
     return (
         <div>
-            <h1>Sign In</h1>
+            {showLogin? 
+                (
+                    <>
+                    <h1>Sign In</h1>
+                    
+                    <p>
+                        New to this app? 
+                        <button onClick={() => setShowLogin(!showLogin)}>Sign Up Here!</button>
+                    </p>
+                    </>
+            ) : (
+                <>
+                    <h1>Sign Up</h1>
+                    <SignUpForm onLogin={onLogin} />
+                    <p>
+                        Already have an account? 
+                        <button onClick={() => setShowLogin(!showLogin)}>Sign In Here!</button>
+                    </p>
+                </>
+            )}
         </div>
     )
 } 
