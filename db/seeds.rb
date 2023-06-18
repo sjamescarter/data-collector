179bad5180bd95888e7f08302867f30aaacc1d8b
sjamescarter <stephenjamescarter@gmail.com>
2023-06-17 20:03:17 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 puts "Seeding 🌱"
 
 # User Seed Data
-User.create(email: "slimjim25@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Jim", last_name: "Thompson", job_title: "DHH Teacher")
-User.create(email: "lannette.simes@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Lannette", last_name: "Simes", job_title: "Physical Therapist")
-User.create(email: "janice-jones@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Janice", last_name: "Jones", job_title: "Speech/Language Pathologist")
+# User.create(email: "slimjim25@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Jim", last_name: "Thompson", job_title: "DHH Teacher")
+# User.create(email: "lannette.simes@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Lannette", last_name: "Simes", job_title: "Physical Therapist")
+# User.create(email: "janice-jones@example.com", password: "pas5W0rd", password_confirmation: "pas5W0rd", first_name: "Janice", last_name: "Jones", job_title: "Speech/Language Pathologist")
 
 puts "Done Seeding 🌾"
