0d5c830ac888fff4d55009bbb1b588d8c07793dc
sjamescarter <stephenjamescarter@gmail.com>
2023-06-21 14:05:58 -0500

    add has_many associations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+  has_many :goals
+  has_many :users, through: :goals
+  
   validates :first_name, :last_name, presence: true
   validates :grade_level, numericality: { in: 1..12 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :goals
+  has_many :students, through: :goals
 
   validates :first_name, :last_name, :job_title, :email, :password_confirmation, presence: true
   validates :email, format: { with: /\A[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}\z/}, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 } 
