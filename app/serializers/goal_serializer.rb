35ffd0f8470d7eda9f5471b79110bd205b2e8817
sjamescarter <stephenjamescarter@gmail.com>
2023-06-21 17:26:37 -0500

    add result calculation

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,5 +1,13 @@
 class GoalSerializer < ActiveModel::Serializer
-  attributes :id, :condition, :behavior, :accuracy, :measurement, :trials_correct, :trials_total
+  attributes :id, :condition, :behavior, :accuracy, :measurement, :result
   has_one :user
   has_one :student
+
+  def result
+    if self.object.trials_total
+      correct = self.object.trials_correct.to_f
+      total = self.object.trials_total.to_f
+      "#{(correct/total * 100).round}%"
+    end
+  end
 end
