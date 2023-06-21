93a143a6e1a3162d9db86ecb9167b405a60ed19e
sjamescarter <stephenjamescarter@gmail.com>
2023-06-21 11:17:04 -0500

    update validations for grade level

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ApplicationRecord
   validates :first_name, :last_name, presence: true
-  validates :grade_level, numericality: { in: -1..12 }
+  validates :grade_level, numericality: { in: 1..12 }
 end
