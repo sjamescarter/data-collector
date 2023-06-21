830ba4acfcd2197006196f39fe24fb67ee195eba
sjamescarter <stephenjamescarter@gmail.com>
2023-06-21 13:49:41 -0500

    add user logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,20 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::Serialization
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session }
+  before_action :find_user
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
+
+  private
+  def find_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      render json: { errors: ["Please Sign In"] }, status: :unauthorized
+    end
+  end
+
+  def invalid_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
