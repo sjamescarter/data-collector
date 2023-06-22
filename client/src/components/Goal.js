9cc75729c0585cd91b754987edb781c843e0a09b
sjamescarter <stephenjamescarter@gmail.com>
2023-06-22 11:22:05 -0500

    add goal editor state and component

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import styled from "styled-components";
+import GoalEditor from './GoalEditor';
 
-function Goal({ goal, student, onDelete }) {
+function Goal({ goal, student, onDelete, handleUpdate }) {
+    const [isEditing, setIsEditing] = useState(false)
 
     return (
         <GoalGrid>
-            <p>Given {goal.condition}, {student.name.split(" ")[0]} will {goal.behavior} with {goal.accuracy}% accuracy as measured by {goal.measurement} by the next annual review.</p>
+            {isEditing 
+                ? <GoalEditor student={student} goal={goal} setIsEditing={setIsEditing} handleUpdate={handleUpdate} />
+                : <p>Given {goal.condition}, {student.name.split(" ")[0]} will {goal.behavior} with {goal.accuracy}% accuracy as measured by {goal.measurement} by the next annual review.</p>
+            }
             <div></div>
             <i className="material-icons">addchart</i>
-            <i className="material-icons">edit</i>
+            <i onClick={() => setIsEditing(!isEditing)} className="material-icons">edit</i>
             <i onClick={() => onDelete(goal.id, student.id)} className="material-icons">delete</i>
             {goal.result ? <p>Student results: {goal.result}</p> : null}
         </GoalGrid>
