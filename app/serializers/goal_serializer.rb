2133c222f294123cbcf1a859663d0d18f2bfa531
sjamescarter <stephenjamescarter@gmail.com>
2023-06-28 15:13:39 -0500

    add owner method

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,12 +1,16 @@
 class GoalSerializer < ActiveModel::Serializer
-  attributes :id, :condition, :behavior, :accuracy, :measurement, :result
-  has_one :user
+  attributes :id, :owner, :condition, :behavior, :accuracy, :measurement, :result
   has_one :student
 
+  def owner
+    owner = User.find(object.user_id)
+    "#{owner.first_name} #{owner.last_name}, #{owner.job_title}"
+  end
+
   def result
-    if self.object.trials_total
-      correct = self.object.trials_correct.to_f
-      total = self.object.trials_total.to_f
+    if object.trials_total
+      correct = object.trials_correct.to_f
+      total = object.trials_total.to_f
       "#{(correct/total * 100).round}%"
     end
   end
