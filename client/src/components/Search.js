c37353ceccfe14a457f5e46949e141ac11f2db46
sjamescarter <stephenjamescarter@gmail.com>
2023-06-28 15:19:44 -0500

    create Search component

---
+++ client/src/components/Search.js
@@ -0,0 +1,25 @@
+import { InputContainer, InputIcon, InputField } from '../styles/';
+
+
+function Search({ children, search, setSearch }) {
+    return (
+        <div style={{width: '80%', margin: 'auto'}}>
+            <h3>Find Student</h3>
+            <InputContainer>
+                <InputIcon className="material-icons input-icon">search</InputIcon>     
+                <InputField 
+                    type="text" 
+                    name="search" 
+                    style={{backgroundColor: "#f8f8f8"}}
+                    placeholder="Student" 
+                    autoComplete="off"
+                    value={search} 
+                    onChange={(e) => setSearch(e.target.value)} 
+                    />
+            </InputContainer>
+            {children}
+        </div>
+    );
+}
+
+export default Search;
