4e58d29fe6cb584e7171606cab524b6890f3f1d8
sjamescarter <stephenjamescarter@gmail.com>
2023-06-28 15:23:47 -0500

    refactor editing

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -6,12 +6,49 @@ import ResultsEditor from './ResultsEditor';
 function Goal({ goal, student, onDelete, handleUpdate }) {
     const [isEditing, setIsEditing] = useState(false)
     const [editResults, setEditResults] = useState(false)
+    const [errors, setErrors] = useState([]);
+
+    function handleSubmit(e, goalForm) {
+        e.preventDefault();
+        setErrors([]);
+        fetch('/goals/' + goal.id, {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(goalForm)
+        })
+        .then((r) => {
+            if (r.ok) {
+                r.json().then((goal) => handleUpdate(student.id, goal));
+                setIsEditing(false);
+            } else {
+                r.json().then((err) => setErrors(err.errors))
+            }
+        })
+    }
 
     return (
         <GoalGrid>
             {isEditing 
-                ? <GoalEditor student={student} goal={goal} setIsEditing={setIsEditing} handleUpdate={handleUpdate} />
-                : <p>Given {goal.condition}, {student.name.split(" ")[0]} will {goal.behavior} with {goal.accuracy}% accuracy as measured by {goal.measurement} by the next annual review.</p>
+                ? <GoalEditor 
+                    student={student} 
+                    goal={goal} 
+                    setIsEditing={setIsEditing} 
+                    onSubmit={handleSubmit}
+                >
+                    <div>
+                        <StyledSubmit type="submit" value="Save" />
+                        <Button onClick={() => setIsEditing(false)}>Cancel</Button>
+                    </div>
+                    <ul className="errors">
+                        {errors ? errors.map((error) => <li key={error}>{error}</li>) : null}
+                    </ul>
+                </GoalEditor>
+                : <div>
+                    <h5>{goal.owner}'s Goal</h5>
+                    <p>Given {goal.condition}, {student.name.split(" ")[0]} will {goal.behavior} with {goal.accuracy}% accuracy as measured by {goal.measurement} by the next annual review.</p>
+                </div>
             }
             <div></div>
             <i onClick={() => setEditResults(!editResults)} className="material-icons">addchart</i>
@@ -36,5 +73,27 @@ const GoalGrid = styled.div`
     padding: 0 15px;
     margin-bottom: 1em;
 `
+const StyledSubmit = styled.input`
+    background-color: #6a8532;
+    color: white;
+    border: 1.5px solid #6a8532; 
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: .8em;
+    padding: 5px 10px;
+    margin: 5px;
+    cursor: pointer;
+`
 
+const Button = styled.button`
+    background-color: #f8f8f8;
+    color: #6a8532;
+    border: 1.5px solid #6a8532; 
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: .8em;
+    padding: 5px 10px;
+    margin: 5px;
+    cursor: pointer;
+`
 export default Goal;
