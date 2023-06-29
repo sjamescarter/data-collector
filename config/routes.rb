b6a7025090d16918c5f2e659b5b4f3a49336d7c3
sjamescarter <stephenjamescarter@gmail.com>
2023-06-29 11:39:39 -0500

    add my_students resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :goals
   resources :students, only: [:index, :create, :show]
+  resources :my_students
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
   post '/signup', to: 'users#create'
-
 
   get '*path', 
     to: 'fallback#index',
