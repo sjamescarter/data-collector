249d6651ab32c207db0e3016008476e474aa6f50
sjamescarter <stephenjamescarter@gmail.com>
2023-06-30 23:39:46 -0500

    add Container

--- client/src/pages/Student.js
+++ client/src/pages/Student.js
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import Goal from "../components/Goal";
 
-function Student({ students, setStudents }) {
+function Student({ user, students, setStudents }) {
     const { id } = useParams();
     const student = students.find((student) => student.id === parseInt(id));
-    const orderedGoals = [...student.goals.sort((a, b) => a.id - b.id)];
-    
+    const filteredGoals = [...student.goals.filter((goal) => goal.user_id === user.id)];
+    const orderedGoals = [...filteredGoals.sort((a, b) => a.id - b.id)];
+
     function handleUpdate(studentId, updatedGoal) {
         const student = [...students].find((student) => student.id === studentId)
         const goals = student.goals.filter((goal) => goal.id !== updatedGoal.id)
@@ -37,17 +39,31 @@ function Student({ students, setStudents }) {
     return (
         <div>
             <h1>{student.name}</h1>
-            {orderedGoals.map((goal) => 
+            <Container>
+                {orderedGoals.map((goal) => 
                     <Goal 
-                        key={goal.id} 
-                        goal={goal} 
-                        student={student} 
-                        onDelete={handleDelete} 
-                        handleUpdate={handleUpdate} 
-                    />) 
-            }
+                    key={goal.id} 
+                    goal={goal} 
+                    student={student} 
+                    onDelete={handleDelete} 
+                    handleUpdate={handleUpdate} 
+                />)}
+            </Container>
         </div>
     );
 }
 
+const Container = styled.div`
+    background-color: #d7dace;
+    border-radius: 4px;
+    width: 80%;
+    padding: .25em 1.2em;
+    font-size: 1.25em;
+    margin: auto;
+    margin-top: .5em;
+    opacity: .9;
+    &:hover {
+        opacity: 1;
+    }
+`
 export default Student;
