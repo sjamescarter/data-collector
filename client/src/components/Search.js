5dec69d0d571ddde4f31c454d4d3c19ca90c7152
sjamescarter <stephenjamescarter@gmail.com>
2023-07-04 12:26:28 -0500

    update new student icon

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,15 @@
-import { InputContainer, InputIcon, InputField } from '../styles/';
+import { useNavigate } from 'react-router-dom';
+import { I, InputContainer, InputIcon, InputField } from '../styles/';
 
 
 function Search({ children, search, setSearch }) {
+    const navigate = useNavigate();
+
     return (
         <div style={{width: '80%', margin: 'auto'}}>
-            <h3>Find Student</h3>
+            <h3>Search Students</h3>
             <InputContainer>
-                <InputIcon className="material-icons input-icon">search</InputIcon>     
+                <InputIcon className="material-icons">search</InputIcon>     
                 <InputField 
                     type="text" 
                     name="search" 
@@ -15,7 +18,14 @@ function Search({ children, search, setSearch }) {
                     autoComplete="off"
                     value={search} 
                     onChange={(e) => setSearch(e.target.value)} 
-                    />
+                />
+                <I 
+                    onClick={() => navigate(`/students/new/${search}`)}
+                    title="New Student"
+                    className="material-icons"
+                >
+                    person_add_alt_1
+                </I>
             </InputContainer>
             {children}
         </div>
