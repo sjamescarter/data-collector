f6884308fcf253d2b31349cf91d7ebc698f6e2f0
sjamescarter <stephenjamescarter@gmail.com>
2023-07-04 12:20:07 -0500

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,48 @@ import NewStudent from "./pages/NewStudent";
 import Student from "./pages/Student"
 
 function App() {
-  const [user, setUser] = useState("");
-  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState();
+  const [students, setStudents] = useState();
 
   useEffect(() => {
     fetch("/me")
     .then((r) => {
       if (r.ok) {
-      r.json().then((user) => setUser(user));
+        r.json().then((user) => handleLogin(user));
       }
     })
-    fetch("/students")
-    .then((r) => {
-      if (r.ok) {
-        r.json().then((students) => setStudents(students));
-      }
-    })
+    // fetch(fetcher)
+    // .then((r) => {
+    //   if (r.ok) {
+    //     r.json().then((students) => setStudents(students));
+    //   }
+    // })
   }, []);
 
-  if (!user) return <Login onLogin={setUser} />
+  function handleLogin(user) {
+    setUser({ 
+      id: user.id, 
+      firstName: user.first_name, 
+      lastName: user.last_name, 
+      jobTitle: user.job_title 
+    });
+    setStudents(user.students);
+  }
 
+  if (!user) return <Login onLogin={handleLogin} />
+console.log(user)
+console.log(students)
   return (
     <BrowserRouter>
         <NavBar user={user} setUser={setUser} />
         <div style={{marginLeft: "12.5em"}}>
           <Routes>
-            <Route path="/" element={<Dashboard students={students} setStudents={setStudents } />} />
+            <Route path="/" element={<Dashboard students={students} setStudents={setStudents} />} />
             <Route path="/students" element={<MyStudents user={user} students={students} setStudents={setStudents} />} />
             <Route path="/students/:id" element={<Student user={user} students={students} setStudents={setStudents} />} />
             <Route path="/students/:id/goals/new" element={<NewGoal students={students} setStudents={setStudents} />} />
             <Route path="/students/new" element={<NewStudent students={students} setStudents={setStudents} />} />
+            <Route path="/students/new/:name" element={<NewStudent students={students} setStudents={setStudents} />} />
             <Route path="/goals/new" element={<NewGoal students={students} setStudents={setStudents} />} />
           </Routes>
         </div>
