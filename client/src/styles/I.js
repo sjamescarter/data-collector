f66fc8f59a9d9896dfcf780f6d98dabf578608c0
sjamescarter <stephenjamescarter@gmail.com>
2023-07-04 12:11:54 -0500

    create I.js

---
+++ client/src/styles/I.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const I = styled.i`
+    padding: 4px;
+    margin: 4px;
+    &:hover {
+        color: #6a8532;
+        cursor: pointer;
+    }
+`
+
+export default I;
