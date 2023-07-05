a235ad7a957dfe59ad96ed80db1d878185b954ef
sjamescarter <stephenjamescarter@gmail.com>
2023-07-05 17:09:16 -0500

    remove add student button

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,31 +1,20 @@
-import { useNavigate } from 'react-router-dom';
-import { I, InputContainer, InputIcon, InputField } from '../styles/';
-
+import { InputContainer, InputIcon, InputField } from '../styles/';
 
 function Search({ children, search, setSearch }) {
-    const navigate = useNavigate();
-
     return (
         <div style={{width: '80%', margin: 'auto'}}>
-            <h3>Search Students</h3>
+            <h3>Search</h3>
             <InputContainer>
                 <InputIcon className="material-icons">search</InputIcon>     
                 <InputField 
                     type="text" 
                     name="search" 
                     style={{backgroundColor: "#f8f8f8"}}
-                    placeholder="Student" 
+                    placeholder="Search Student Name" 
                     autoComplete="off"
                     value={search} 
                     onChange={(e) => setSearch(e.target.value)} 
                 />
-                <I 
-                    onClick={() => navigate(`/students/new/${search}`)}
-                    title="New Student"
-                    className="material-icons"
-                >
-                    person_add_alt_1
-                </I>
             </InputContainer>
             {children}
         </div>
