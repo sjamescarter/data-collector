962035ede36df80868c3e8c5af9674a2bbb00a07
sjamescarter <stephenjamescarter@gmail.com>
2023-07-05 19:10:42 -0500

    comment out unused code

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,12 +1,19 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :initial, :name, :grade_level
-  has_many :goals
+  has_many :goals 
+  # do
+  #   object.goals.where(user_id: $current_user) 
+  # end
+  
+  def initial
+    object.first_name[0]
+  end
 
   def name
     "#{object.first_name} #{object.last_name}"
   end
 
-  def initial
-    object.first_name[0]
-  end
+  # def current_user
+  #   @@user = @instance_options[:current_user].id
+  # end
 end
