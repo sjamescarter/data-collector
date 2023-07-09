93d88c063abb8c52c318ee92e6cc781e26deb095
sjamescarter <stephenjamescarter@gmail.com>
2023-07-09 14:44:59 -0500

    create Header and Li components

---
+++ client/src/styles/Header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+    align-items: center;
+    border-bottom: 1px solid #999;
+    display: grid;
+    font-size: 1.25em;
+    grid-template-columns: 3fr 140px;
+    margin: auto;
+    padding: 10px;
+    width: 80%;
+`
+
+export default Header;
---
+++ client/src/styles/Li.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Li = styled.li`
+    list-style: none;
+    cursor: pointer;
+`
+
+export default Li;
