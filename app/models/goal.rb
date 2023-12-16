aceb660769f49e40779a376971b1e81cbb0d7925
sjamescarter <stephenjamescarter@gmail.com>
2023-12-16 12:13:08 -0600

    Refactor

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,7 @@ class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :student
 
-  validates :condition, :behavior, :accuracy, :measurement, presence: :true
+  validates :condition, :behavior, :accuracy, :measurement, presence: true
   validates :accuracy, numericality: { in: 0..100 }
   validates :trials_correct, numericality: { allow_nil: true }
   validates :trials_total, comparison: { greater_than_or_equal_to: :trials_correct, allow_nil: true }
