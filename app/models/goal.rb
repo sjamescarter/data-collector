e9ccc30d809c945d223a5bdaae528360e42b4d3f
sjamescarter <stephenjamescarter@gmail.com>
2024-01-20 16:19:59 -0600

    Add has_many :assessments

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,6 +2,7 @@ class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :student
   has_many :objectives
+  has_many :assessments, through: :objectives
 
   validates :condition, :behavior, :accuracy, :measurement, presence: true
   validates :accuracy, numericality: { in: 0..100 }
