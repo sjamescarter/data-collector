b576170913242acad1755b55f82ae3e094738e7d
sjamescarter <stephenjamescarter@gmail.com>
2024-01-23 10:14:51 -0600

    Create EditButtons.js

---
+++ client/src/components/EditButtons.js
@@ -0,0 +1,34 @@
+import { I } from "../styles";
+
+function EditButtons({ addData, title, editAction, deleteAction }) {
+    return (
+        <div className="flex">
+            {addData
+                ? <I 
+                className="material-icons"
+                onClick={addData}
+                title="Add Data"
+                >
+                add_chart
+                </I>
+                : null
+            }
+            <I 
+                className="material-icons"
+                onClick={editAction}
+                title={`Edit ${title}`}
+            >
+                edit
+            </I>
+            <I 
+                className="material-icons"
+                onClick={deleteAction} 
+                title={`Delete ${title}`}
+            >
+                delete
+            </I>
+        </div>
+    );
+}
+
+export default EditButtons;
