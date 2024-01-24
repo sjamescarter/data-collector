f379f87be4bfc0078f95e2d2a1a42db9670102ac
sjamescarter <stephenjamescarter@gmail.com>
2024-01-24 17:46:34 -0600

    Add createObjective modal and function

--- client/src/components/Objectives.js
+++ client/src/components/Objectives.js
@@ -1,22 +1,56 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { UserContext } from "../context/user";
 import styled from "styled-components";
-import { I } from "../styles";
+import { I, InputSubmit } from "../styles";
+import { submit } from "./fetch";
 import ObjectiveCard from "./ObjectiveCard";
 import Modal from "./Modal";
+import { handleChange } from "./utilities";
 
-function Objectives({ goal }) {
+function Objectives({ student, goal }) {
     const { id, objectives } = goal;
     const { user, setUser, students, setStudents } = useContext(UserContext);
+    const [objectiveForm, setObjectiveForm] = useState({description: `Given ${goal.condition}, ${student.name.split(" ")[0]} will ${goal.behavior} with ${goal.accuracy}% accuracy as measured by ${goal.measurement} by the next annual review.`})
+    const [errors, setErrors] = useState();
     const createObjective = useRef(null);
 
     // Create Objective
-    // function 
-    // Update Objective
-    // Delete Objective
+    function handleSubmit(e, objectiveForm) {
+        e.preventDefault();
+        setErrors();
+
+        const callback = (o) => {
+            setUser({
+                ...user, students: [
+                    ...user.students.map((s) => s.id === student.id
+                        ? { ...s, goals: [
+                            ...s.goals.map((g) => g.id === id
+                                ? { ...g, objectives: [...g.objectives, o] }
+                                : g
+                            )
+                        ]} 
+                        : s
+                    )
+                ]
+            });
+            setStudents([
+                ...students.map((s) => s.id === student.id
+                    ? { ...s, goals: [
+                        ...s.goals.map((g) => g.id === id
+                            ? { ...g, objectives: [...g.objectives, o] }
+                            : g
+                        )
+                    ]} 
+                    : s
+                )
+            ]);
+            createObjective.current.close();
+        }
+        submit(`/goals/${id}/objectives`, 'POST', objectiveForm, callback, setErrors);
+    } 
 
     return (
-        <div>
+        <>
             <div style={{display: "flex", alignItems: "center", borderBottom: "solid 2px #d7dace", justifyContent: "space-between"}}>
                 <div className="flex">
                     <I className="material-icons">task_alt</I>
@@ -37,14 +71,28 @@ function Objectives({ goal }) {
                     <p style={{padding: '0 4px 0 0'}}>New Objective</p>
                 </Button>
             </div>
-            {objectives
-                ? objectives.map((obj) => <ObjectiveCard key={obj.id} objective={obj} />)
+            {objectives.length > 0
+                ? objectives.map((obj) => <ObjectiveCard key={obj.id} objective={obj} goalID={id} />)
                 : null
             }
             <Modal ref={createObjective}>
-
+                <div>
+                    <form onSubmit={(e) => handleSubmit(e, objectiveForm)}>
+                        <h1>Create Objective</h1>
+                        <textarea 
+                            name="description" 
+                            rows={10}
+                            value={objectiveForm.description} 
+                            onChange={(e) => handleChange(objectiveForm, setObjectiveForm, e)} 
+                            />
+                        <InputSubmit type="Submit" value="Submit" />
+                        <ul className="errors">
+                            {errors ? errors.map((error) => <li key={error}>{error}</li>) : null}
+                        </ul>
+                    </form>
+                </div>
             </Modal>
-        </div>
+        </>
     );
 }
 
