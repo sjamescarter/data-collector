00d4462d326cc7d4ade1b25638d39a431cb2aafd
sjamescarter <stephenjamescarter@gmail.com>
2024-01-31 09:31:14 -0600

    Refactor owner method, create summary method

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,10 +1,12 @@
 class GoalSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :owner, :subject, :condition, :behavior, :accuracy, :measurement
+  attributes :id, :user_id, :owner, :summary, :subject, :condition, :behavior, :accuracy, :measurement
   has_many :objectives
 
   def owner
-    owner = User.find(object.user_id)
-    "#{owner.first_name} #{owner.last_name}, #{owner.job_title}"
+    "#{object.user.first_name} #{object.user.last_name}, #{object.user.job_title}"
   end
 
+  def summary
+    "Given #{object.condition}, #{object.student.first_name} will #{object.behavior} with #{object.accuracy}% accuracy as measured by #{object.measurement} by the next annual review."
+  end
 end
