ec2f34eade5fc4017062413310bcf55202d6b837
sjamescarter <stephenjamescarter@gmail.com>
2024-01-31 10:01:20 -0600

    Refactor

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,60 +1,52 @@
 import { useState } from 'react';
 import { handleChange } from './utilities';
 import Errors from './Errors';
+import { submit } from './fetch';
 
 function LoginForm({ onLogin, loadApp }) {
     const [form, setForm] = useState({email: "", password: ""});
-    const [errors, setErrors] = useState([]);
+    const [errors, setErrors] = useState();
 
     function handleSubmit(e) {
         e.preventDefault();
-        fetch("/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(form)
-        })
-        .then((r) => {
-            if (r.ok) {
-                r.json().then((user) => onLogin(user));
-                setForm({email: "", password: ""});
-                loadApp();
-            } else {
-                r.json().then((err) => setErrors(err.errors));
-            }
-        });
+        setErrors();
+
+        const callback = (user) => {
+            onLogin(user);
+            setForm({email: "", password: ""});
+            loadApp();
+        }
+
+        submit('/login', 'POST', form, callback, setErrors);
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <i className="material-icons input-icon">mail</i>
-                    <input 
-                        type="text" 
-                        name="email" 
-                        className="input-field" 
-                        placeholder="Email" 
-                        value={form.email} 
-                        onChange={(e) => handleChange(form, setForm, e)} 
-                    />
-                </div>
-                <div className="input-container">
-                    <i className="material-icons input-icon">lock</i>
-                    <input 
-                        type="password" 
-                        name="password" 
-                        className="input-field" 
-                        placeholder="Password" 
-                        value={form.password} 
-                        onChange={(e) => handleChange(form, setForm, e)} 
-                    /> 
-                </div>
-                <input type="submit" className="input-submit" value="Sign In" />
-            </form>
+        <form onSubmit={handleSubmit}>
+            <div className="input-container">
+                <i className="material-icons input-icon">mail</i>
+                <input 
+                    type="text" 
+                    name="email" 
+                    className="input-field" 
+                    placeholder="Email" 
+                    value={form.email} 
+                    onChange={(e) => handleChange(form, setForm, e)} 
+                />
+            </div>
+            <div className="input-container">
+                <i className="material-icons input-icon">lock</i>
+                <input 
+                    type="password" 
+                    name="password" 
+                    className="input-field" 
+                    placeholder="Password" 
+                    value={form.password} 
+                    onChange={(e) => handleChange(form, setForm, e)} 
+                /> 
+            </div>
+            <input type="submit" className="input-submit" value="Sign In" />
             <Errors errors={errors} />
-        </div>
+        </form>
     );
 }
 
