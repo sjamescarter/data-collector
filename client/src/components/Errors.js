41b9d9d884e9f26aab25894280ffb4690225c28a
sjamescarter <stephenjamescarter@gmail.com>
2024-02-07 16:25:52 -0600

    Refactor

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,9 +1,33 @@
+import styled from "styled-components";
+import { I } from "../styles";
+
 function Errors({ errors }) {
     return (
-        <ul className="errors">
-            {errors ? errors.map((error) => <li key={error}>{error}</li>) : null}
-        </ul>
+        <div>
+            {errors 
+                ? errors.map((error) => 
+                    <Error 
+                        key={error}
+                    >
+                        <I className="material-icons">warning</I>
+                        {error}
+                    </Error>
+                ) 
+                : null
+            }
+        </div>
     );
 }
 
+const Error = styled.li`
+    background-color: #eb5e28;
+    border-radius: 4px;
+    box-sizing: border-box;
+    color: #f8f8f8;
+    display: flex;
+    gap: 6px;
+    list-style: none;
+    margin: 5px;
+    padding: 4px;
+`
 export default Errors;
--- client/src/components/GoalEditor.js
+++ client/src/components/GoalEditor.js
@@ -1,73 +1,120 @@
-import { useState } from 'react'
 import styled from 'styled-components';
 import { handleChange } from './utilities';
+import { InputField } from '../styles';
 
-function GoalEditor({ children, student, goal, onSubmit }) {
-    const [goalForm, setGoalForm] = useState(goal);
-
-    function calcWidth(target) {
-        return (target.length > 13 ? target.length : 13);
-    }
+function GoalEditor({ children, student, goalForm, setGoalForm, onSubmit, cancel }) {
+    const { subject, condition, behavior, accuracy, measurement } = goalForm;
+    const onChange = (e) => handleChange(goalForm, setGoalForm, e);
 
     return (
-        <form style={{boxSizing: "border-box", maxWidth: "500px"}} onSubmit={(e) => onSubmit(e, goalForm)}>
-            <h3>{goal.subject} Goal</h3>
-            Given 
-            <InputField 
-                type="text" 
-                name="condition" 
-                placeholder="condition"
-                style={{width: `${calcWidth(goalForm.condition)}ch`}}
-                value={goalForm.condition} 
-                onChange={(e) => handleChange(goalForm, setGoalForm, e)} 
-                />,
-            {` ${student.name.split(" ")[0]} `} 
-            will
-            <InputField 
-                type="text" 
-                name="behavior" 
-                placeholder="behavior"
-                style={{width: `${calcWidth(goalForm.behavior)}ch`}}
-                value={goalForm.behavior} 
-                onChange={(e) => handleChange(goalForm, setGoalForm, e)} 
-                />
-            with
-            <InputField 
-                type="number" 
-                name="accuracy" 
-                placeholder="0"
-                style={{width: "6ch"}} 
-                value={goalForm.accuracy} 
-                onChange={(e) => handleChange(goalForm, setGoalForm, e)} 
-                />
-            % accuracy as measured by 
-            <InputField 
-                type="text" 
-                name="measurement" 
-                placeholder="measurement"
-                style={{width: `${calcWidth(goalForm.measurement)}ch`}}
-                value={goalForm.measurement} 
-                onChange={(e) => handleChange(goalForm, setGoalForm, e)} 
-                />
-            by the next annual review.
-            {children}
-        </form>
+        <Form onSubmit={onSubmit}>
+            <div style={{textAlign: "left"}}>
+                <Label>
+                    SUBJECT
+                    <InputField 
+                        type="text"
+                        name="subject"
+                        placeholder="Reading, Writing..."
+                        value={subject}
+                        onChange={onChange}
+                    />
+                </Label>
+                <Label>
+                    CONDITION
+                    <TextArea 
+                        name="condition" 
+                        placeholder="a reading passage..."
+                        rows='3'
+                        value={condition} 
+                        onChange={onChange}
+                    />
+                </Label>
+                <Label>
+                    BEHAVIOR
+                    <TextArea 
+                        name="behavior" 
+                        placeholder="correctly identify..."
+                        rows='2'
+                        value={behavior} 
+                        onChange={onChange}
+                    />
+                </Label>
+                <Label>
+                    ACCURACY %
+                    <InputField 
+                        type="number" 
+                        name="accuracy" 
+                        placeholder="percentage"
+                        value={accuracy} 
+                        onChange={onChange}
+                    />
+                </Label>
+                <Label>
+                    MEASUREMENT
+                    <TextArea 
+                        name="measurement" 
+                        placeholder="teacher records..."
+                        rows='2'
+                        value={measurement} 
+                        onChange={onChange}
+                    />
+                </Label>
+            </div>
+            <Summary>
+                <GoalTitle>{goalForm.subject} Goal</GoalTitle>
+                <P>Given {goalForm.condition || 'CONDITION'}, {student.name.split(" ")[1]} will {goalForm.behavior || 'BEHAVIOR'} with {goalForm.accuracy || '###'}% accuracy as measured by {goalForm.measurement || 'MEASUREMENT'} by the next annual review.</P>
+                {children}
+                <Cancel type='button' onClick={cancel}>Cancel</Cancel>
+            </Summary>
+        </Form>
     );
 }
 
-const InputField = styled.input`
+const Form = styled.form`
+    display: flex;
+    box-sizing: border-box;
+    gap: 2em;
+`
+const Label = styled.label`
+`
+const GoalTitle = styled.h3`
+    border-bottom: 2px solid #999;
+    padding: 0 0 1em 0;
+`
+const P = styled.p`
+    margin-bottom: 3em;
+`
+const TextArea = styled.textarea`
     background-color: #f8f8f8;
     border: none;
     border-bottom: 1px solid #999;
     box-sizing: border-box;
+    border-radius: 4px;
     font-family: 'Ubuntu';
     font-size: 1em;
-    padding: 5px;
-    margin: 5px 5px;
-    text-align: center;
+    padding: 15px;
+    margin: 5px 0 20px;
+    width: 100%;
     &:focus {
         outline-color: #6a8532;
     }
 `
-
+const Summary = styled.div`
+    width: 200%;
+    font-size: 1.1em;
+`
+const Cancel = styled.button`
+    border: 2px solid #6a8532;
+    border-radius: 4px;
+    color: #6a8532;
+    font-weight: bold;
+    font-size: .8em;
+    margin: 20px;
+    padding: 10px;
+    background-color: #f8f8f8;
+    &:hover {
+        background-color: #f8f8f8;
+        cursor: pointer;
+    }
+`
 export default GoalEditor;
