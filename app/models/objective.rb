344acd22fb7ea78b8116888020863e5646eb2d5c
sjamescarter <stephenjamescarter@gmail.com>
2024-02-07 16:10:16 -0600

    Define scope

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -2,5 +2,7 @@ class Objective < ApplicationRecord
   belongs_to :goal
   has_many :assessments, dependent: :destroy
 
+  default_scope { order(created_at: :asc) }
+
   validates :description, presence: true
 end
